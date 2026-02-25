77e4a2ccb11795b1dcfd3d8402a7b4765b03cacc
missipsa <missipsabhd@gmail.com>
2026-02-25 22:03:50 +0100

    restore home controller

--- lib/controllers/HomeController.dart
+++ lib/controllers/HomeController.dart
@@ -1,71 +1,55 @@
-import 'dart:async';
+import 'package:dinopet_walker/controllers/DinoController.dart';
+import 'package:dinopet_walker/database/dao/DailyStepsDao.dart';
 import 'package:dinopet_walker/services/PedometerService.dart';
+import 'package:dinopet_walker/utils/DateFormatter.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class HomeController extends ChangeNotifier {
+  DinoController dinoController;
+
   late PedometerService _pedometerService;
 
   int currentSteps = 0;
-  int userLevel = 10;
   final int goalSteps = 10000;
+  bool _isInitialized = false;
 
-  StreamSubscription<int>? _stepsSubscription;
-  bool _isInitialized = false;// on initialise le controlleur qu'une seule fois
+  HomeController({required this.dinoController});
 
   Future<void> init() async {
-    if (_isInitialized) return; 
+    if (_isInitialized) return;
+    _isInitialized = true;
 
     _pedometerService = PedometerService();
 
-    //demander les permessions
     await Permission.activityRecognition.request();
     await Permission.notification.request();
-
-    // pour ignorer exempter l'app de l'optimisation de batterie
     if (await Permission.ignoreBatteryOptimizations.isDenied) {
       await Permission.ignoreBatteryOptimizations.request();
     }
 
-    _initService();
-    _isInitialized = true;
-  }
+    await _pedometerService.initialize();
+    currentSteps = _pedometerService.todaySteps;
 
-  void _initService() {
-    Future(() async {
-      await _pedometerService.initialize();
+    dinoController.dinoPet?.addSteps(currentSteps);
+    notifyListeners();
 
-      currentSteps = _pedometerService.todaySteps;
+    _pedometerService.stepsStream.listen((steps) {
+      final difference = steps - currentSteps;
+      currentSteps = steps;
+      if (difference > 0) dinoController.dinoPet?.addSteps(difference);
       notifyListeners();
-
-      // Écouter les mises à jour des pas
-      _stepsSubscription = _pedometerService.stepsStream.listen((steps) {
-        currentSteps = steps;
-        notifyListeners();
-      });
+      dinoController.notifyListeners();
     });
   }
 
-
-  void increaseLevel() {
-    userLevel += 5;
+  Future<void> addSteps(int steps) async {
+    dinoController.addSteps(steps);
     notifyListeners();
-  }
-
-  void decreaseLevel() {
-    if (userLevel > 5) {
-      userLevel -= 5;
-      notifyListeners();
-    }
-  }
-
-  Future<void> stopService() async {
-    await _pedometerService.stopService();
   }
 
   @override
   void dispose() {
-    _stepsSubscription?.cancel();
     _pedometerService.dispose();
     super.dispose();
   }
