47ac99ce853113a34dba6aceeeb5246928005fc3
missipsa <missipsabhd@gmail.com>
2026-02-26 08:30:19 +0100

    add orange dino assets

--- lib/data/dinoData.dart
+++ lib/data/dinoData.dart
@@ -46,16 +46,35 @@ final List<DinoType> availableDinos = [
     },
   ),
   DinoType(
-    id: 'tresa',
-    name: 'Tresa',
+    id: 'falcon',
+    name: 'Falcon',
     description: 'Un dinosaure rose passionné et déterminé.',
-    outColor: const Color.fromARGB(255, 255, 215, 246),
-    innerColor: const Color(0xFFF8BBD0),
+    outColor: const Color(0xFFFFCC80),
+    innerColor: const Color(0xFFFF9800),
     assetPaths: {
-      LifeStage.baby: {1: 'assets/images/dino_vert_bebe.png'},
-      LifeStage.child: {11: 'assets/images/dino_vert_bebe.png'},
-      LifeStage.teenager: {26: 'assets/images/dino_vert_bebe.png'},
-      LifeStage.adult: {41: 'assets/images/dino_vert_bebe.png'},
+      LifeStage.baby: {
+        1: 'assets/images/dinos/orange_dinos/baby/orange_bebe_1.png',
+        3: 'assets/images/dinos/orange_dinos/baby/orange_bebe_2.png',
+        5: 'assets/images/dinos/orange_dinos/baby/orange_bebe_3.png',
+        7: 'assets/images/dinos/orange_dinos/baby/orange_bebe_4.png',
+        10: 'assets/images/dinos/orange_dinos/baby/orange_bebe_5.png',
+      },
+      LifeStage.child: {
+        11: 'assets/images/dinos/orange_dinos/child/orange_child_1.png',
+        15: 'assets/images/dinos/orange_dinos/child/orange_child_1.png',
+        20: 'assets/images/dinos/orange_dinos/child/orange_child_1.png',
+        25: 'assets/images/dinos/orange_dinos/child/orange_child_1.png'
+      },
+      LifeStage.teenager: {
+        26: 'assets/images/dinos/orange_dinos/teenager/orange_teenager_1.png',
+        33: 'assets/images/dinos/orange_dinos/teenager/orange_teenager_2.png',
+        40: 'assets/images/dinos/orange_dinos/teenager/orange_teenager_3.png',
+      },
+      LifeStage.adult: {
+        41: 'assets/images/dinos/orange_dinos/adult/orange_adult_1.png',
+        45: 'assets/images/dinos/orange_dinos/adult/orange_adult_2.png',
+        48: 'assets/images/dinos/orange_dinos/adult/orange_adult_3.png',
+      },
     },
   ),
 ];
