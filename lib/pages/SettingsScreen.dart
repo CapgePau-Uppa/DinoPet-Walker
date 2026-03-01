c07c5102d70087c108a218017883148fe87ac6ff
missipsa <missipsabhd@gmail.com>
2026-03-01 21:29:35 +0100

    add signout function to Settings screen

--- lib/pages/SettingsScreen.dart
+++ lib/pages/SettingsScreen.dart
@@ -1,10 +1,52 @@
+import 'package:dinopet_walker/widgets/login/AuthWrapper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  void Signout() async{
+    print("deconnceté");
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (_) => AuthWrapper()),
+      (route) => false, 
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Paramètres"));
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 200, 
+          height: 50, 
+          child: ElevatedButton(
+            onPressed: () => Signout(),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red, 
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12), 
+              ),
+            ),
+            child: const Text(
+              "Se déconnecter",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
