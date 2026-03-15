f71db6977b8e05876252004ab65ab3d18cae8a23
missipsa <missipsabhd@gmail.com>
2026-03-15 16:21:02 +0100

    display weekly total distance and total time

--- lib/controllers/activity_controller.dart
+++ lib/controllers/activity_controller.dart
@@ -7,6 +7,12 @@ class ActivityController extends ChangeNotifier {
   final StravaService _stravaService = StravaService();
   final _storage = const FlutterSecureStorage();
 
+  double _totalDistance = 0.0;
+  int _totalDuration = 0;
+
+  double get totalDistance => _totalDistance;
+  int get totalDuration => _totalDuration;
+
   List<SportActivity> activities = [];
   bool isLoading = true;
   bool isStravaLinked = false;
@@ -19,7 +25,26 @@ class ActivityController extends ChangeNotifier {
     isStravaLinked = token != null;
 
     if (isStravaLinked) {
-      activities = await _stravaService.fetchActivities();
+      activities = (await _stravaService.fetchActivities()).where((a) => a.type != 'Walk').toList();
+
+      final now = DateTime.now();
+      final weekStart = now.subtract(Duration(days: now.weekday - 1));
+
+      _totalDistance = 0.0;
+      _totalDuration = 0;
+
+      for (final acvt in activities) {
+        final activityDay = DateTime(acvt.date.year, acvt.date.month, acvt.date.day);
+        final weekStartDay = DateTime(weekStart.year,weekStart.month,weekStart.day,);
+        if (!activityDay.isBefore(weekStartDay)) {
+          if (acvt.distanceInKm > 0){
+            _totalDistance += acvt.distanceInKm;
+          } 
+          if (acvt.durationInMinutes > 0){
+            _totalDuration += acvt.durationInMinutes;
+          }  
+        }
+      }
     }
 
     isLoading = false;
