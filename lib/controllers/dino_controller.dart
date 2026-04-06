6470f9621a56919ab432a63baee89a664024dbd3
missipsa <missipsabhd@gmail.com>
2026-04-06 21:19:12 +0200

    update: dino controller, selection screen/controller and goal wrapper to create, save and load dino

--- lib/controllers/dino_controller.dart
+++ lib/controllers/dino_controller.dart
@@ -1,12 +1,31 @@
 import 'package:dinopet_walker/models/dino_pet.dart';
+import 'package:dinopet_walker/models/dino_type.dart';
+import 'package:dinopet_walker/services/dino_service.dart';
 import 'package:flutter/material.dart';
 
 class DinoController extends ChangeNotifier {
   DinoPet? _dinoPet;
   DinoPet? get dinoPet => _dinoPet;
 
-  void initializeDinoPet(DinoPet newDino) {
-    _dinoPet = newDino;
+  bool isLoading = true;
+
+  final DinoPetService _dinoPetService = DinoPetService();
+
+  // Créer l'objet dino et le sauvegarder sur Firestore pour la prémière fois
+  Future<void> initializeAndSave(DinoType type) async {
+    _dinoPet = _dinoPetService.createNewDinoPet(type);
+    notifyListeners();
+    await _dinoPetService.saveDinoPet(_dinoPet!);
+  }
+
+  // Charger le dino depuis Firestore
+  Future<void> loadDinoPet() async {
+    isLoading = true;
+    notifyListeners();
+
+    _dinoPet = await _dinoPetService.loadDinoPet();
+
+    isLoading = false;
     notifyListeners();
   }
 
@@ -18,10 +37,8 @@ class DinoController extends ChangeNotifier {
 
   void resetDino() {
     if (_dinoPet == null) return;
-
     _dinoPet!.level = 1;
     _dinoPet!.xpSteps = 0;
-
     notifyListeners();
   }
 }
